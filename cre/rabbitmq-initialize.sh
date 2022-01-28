7a234a466d92896153e6c4394250acbc431d2d6d
KMicha <michael.kahle@yahoo.de>
2022-01-28 10:26:57 +0100

    use path

--- cre/rabbitmq-initialize.sh
+++ cre/rabbitmq-initialize.sh
@@ -7,13 +7,14 @@ sleep 1
 echo "rabbitmq server running"
 
 wget http://127.0.0.1:15672/cli/rabbitmqadmin
+sleep 3
 chmod +x rabbitmqadmin
-mv rabbitmqadmin /etc/rabbitmqadmin
+mv rabbitmqadmin /etc/rabbitmq/rabbitmqadmin
 
 #munchausen-swamp
-/etc/rabbitmqadmin declare exchange name="cre.swamp" type=direct  durable=false auto_delete=true
-/etc/rabbitmqadmin declare queue name="cre.munchausen" durable=false auto_delete=true
-/etc/rabbitmqadmin declare binding source="cre.swamp" destination="cre.munchausen" routing_key="cre.horse" 
+/etc/rabbitmq/rabbitmqadmin declare exchange name="cre.swamp" type=direct  durable=false auto_delete=true
+/etc/rabbitmq/rabbitmqadmin declare queue name="cre.munchausen" durable=false auto_delete=true
+/etc/rabbitmq/rabbitmqadmin declare binding source="cre.swamp" destination="cre.munchausen" routing_key="cre.horse" 
 ## [destination_type=... arguments=...]
 
 amqp-consume -e "cre.swamp" -q "cre.munchausen" -r "cre.horse" --vhost "/" -d ./create-queue.sh
