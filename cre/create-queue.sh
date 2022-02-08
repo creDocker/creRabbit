c601c70adbb6f88376bc29ba6ae5ab16b0f36d3d
KMicha <michael.kahle@yahoo.de>
2022-02-08 16:26:54 +0100

    json needs real quotes

--- cre/create-queue.sh
+++ cre/create-queue.sh
@@ -14,7 +14,7 @@ delayed=$(/usr/sbin/rabbitmq-plugins list -E rabbitmq_delayed_message_exchange |
 if [ $delayed -eq 0 ]; then
     /etc/rabbitmq/rabbitmqadmin declare exchange name=$exchange type=direct durable=true auto_delete=false
 else
-    /etc/rabbitmq/rabbitmqadmin declare exchange name=$exchange type=x-delayed-message arguments="{'x-delayed-type':'direct'}" durable=true auto_delete=false 
+    /etc/rabbitmq/rabbitmqadmin declare exchange name=$exchange type=x-delayed-message arguments='{"x-delayed-type":"direct"}' durable=true auto_delete=false 
 fi
 
 /etc/rabbitmq/rabbitmqadmin declare queue name=$queue durable=true auto_delete=false
