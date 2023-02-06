37ae5d0a87d9e57f40b169d3c5fe1a1c23151aff
KMicha <michael.kahle@yahoo.de>
2023-02-06 17:48:36 +0100

    dont use empty file

--- cre/rabbit-plugins.sh
+++ cre/rabbit-plugins.sh
@@ -7,7 +7,13 @@ fi
 
 # download file if not exists
 if [ ! -f "/cre/plugins/v$RABBIT_DELAY_VERSION/rabbitmq_delayed_message_exchange-$RABBIT_DELAY_VERSION.ez" ]; then
-    wget "https://github.com/rabbitmq/rabbitmq-delayed-message-exchange/releases/download/$RABBIT_DELAY_VERSION/rabbitmq_delayed_message_exchange-$RABBIT_DELAY_VERSION.ez" -O "/cre/plugins/v$RABBIT_DELAY_VERSION/rabbitmq_delayed_message_exchange-$RABBIT_DELAY_VERSION.ez"
+    wget "https://github.com/rabbitmq/rabbitmq-delayed-message-exchange/releases/download/$RABBIT_DELAY_VERSION/rabbitmq_delayed_message_exchange-$RABBIT_DELAY_VERSION.ez" -O "/cre/plugins/v$RABBIT_DELAY_VERSION/rabbitmq_delayed_message_exchange.tmp.ez"
+    PLUGINSIZE=$(stat -c%s "/cre/plugins/v$RABBIT_DELAY_VERSION/rabbitmq_delayed_message_exchange.tmp.ez")
+    if [ (( $PLUGINSIZE > 1000)) ]; then
+        mv "/cre/plugins/v$RABBIT_DELAY_VERSION/rabbitmq_delayed_message_exchange.tmp.ez" "/cre/plugins/v$RABBIT_DELAY_VERSION/rabbitmq_delayed_message_exchange-$RABBIT_DELAY_VERSION.ez"
+    else
+        rm -f "/cre/plugins/v$RABBIT_DELAY_VERSION/rabbitmq_delayed_message_exchange.tmp.ez"
+    fi 
 fi
 
 
