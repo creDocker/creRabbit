3ce0686145c90aadd98c44b22b3005fd768d45ad
KMicha <michael.kahle@yahoo.de>
2023-02-07 13:10:30 +0100

    more prints

--- cre/rabbit-plugins.sh
+++ cre/rabbit-plugins.sh
@@ -11,13 +11,15 @@ fi
 # download file if not exists
 if [ ! -f "/cre/plugins/v$RABBIT_DELAY_VERSION/rabbitmq_delayed_message_exchange-$RABBIT_DELAY_VERSION.ez" ]; then
     echo "Delayed Message Plugin v$RABBIT_DELAY_VERSION will be downloaded."
-    wget "https://github.com/rabbitmq/rabbitmq-delayed-message-exchange/releases/download/$RABBIT_DELAY_VERSION/rabbitmq_delayed_message_exchange-$RABBIT_DELAY_VERSION.ez" -O "/cre/plugins/v$RABBIT_DELAY_VERSION/rabbitmq_delayed_message_exchange.tmp.ez"
-    PLUGINSIZE=$(stat -c%s "/cre/plugins/v$RABBIT_DELAY_VERSION/rabbitmq_delayed_message_exchange.tmp.ez")
+    wget "https://github.com/rabbitmq/rabbitmq-delayed-message-exchange/releases/download/$RABBIT_DELAY_VERSION/rabbitmq_delayed_message_exchange-$RABBIT_DELAY_VERSION.ez" -O "/cre/plugins/v$RABBIT_DELAY_VERSION/rabbitmq_delayed_message_exchange.ez.tmp"
+    PLUGINSIZE=$(stat -c%s "/cre/plugins/v$RABBIT_DELAY_VERSION/rabbitmq_delayed_message_exchange.ez.tmp")
     if [ $(echo " $PLUGINSIZE > 1000" | bc) -eq 1 ]; then
-        mv "/cre/plugins/v$RABBIT_DELAY_VERSION/rabbitmq_delayed_message_exchange.tmp.ez" "/cre/plugins/v$RABBIT_DELAY_VERSION/rabbitmq_delayed_message_exchange-$RABBIT_DELAY_VERSION.ez"
+        echo "Delayed Message Plugin v$RABBIT_DELAY_VERSION downloaded successfully."
+        mv "/cre/plugins/v$RABBIT_DELAY_VERSION/rabbitmq_delayed_message_exchange.ez.tmp" "/cre/plugins/v$RABBIT_DELAY_VERSION/rabbitmq_delayed_message_exchange-$RABBIT_DELAY_VERSION.ez"
+        echo "$(ls -l /cre/plugins/v$RABBIT_DELAY_VERSION)" 
     else
         echo "Delayed Message Plugin v$RABBIT_DELAY_VERSION does not exist."         
-        rm -f "/cre/plugins/v$RABBIT_DELAY_VERSION/rabbitmq_delayed_message_exchange.tmp.ez"
+        rm -f "/cre/plugins/v$RABBIT_DELAY_VERSION/rabbitmq_delayed_message_exchange.ez.tmp"
     fi 
 else
     echo "Delayed Message Plugin v$RABBIT_DELAY_VERSION exists."
